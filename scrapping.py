550e410a56dfe4b8db31a4166c59ff9e13beec83
jeffery1128 <57884601+jeffery1128@users.noreply.github.com>
2021-09-04 01:54:04 +0800

    Successfully read to dataframe

--- scrapping.py
+++ scrapping.py
@@ -4,14 +4,29 @@ from bs4 import BeautifulSoup
 import time
 driver = webdriver.Chrome()
 driver.get('https://finviz.com/screener.ashx?v=111')
-time.sleep(2)
-html=driver.page_source
-soup=BeautifulSoup(html,'html.parser')
-tb = soup.find_all('table')
-soup=BeautifulSoup(tb[17].text,'html.parser')
-print(type(tb[17]))
-#tb = soup.find_all('tbody')
+total = (driver.find_element_by_xpath('//*[@id="screener-content"]/table/tbody/tr[3]/td/table/tbody/tr/td[1]')).text
+press_time = int(total[7:11])/20
+print(press_time)
+Cat = pd.DataFrame()
+#soup=BeautifulSoup(html,'html.parser')
+#tb = soup.find_all('table')
+#target = tb[16]
+#print(target)
+tb = driver.find_element_by_xpath('//*[@id="screener-content"]/table/tbody/tr[4]/td/table')
+html_tb = tb.get_attribute('innerHTML')
+html_tb = '<table>' + html_tb + '</table>'
+df = pd.read_html(html_tb)
+pd.concat([Cat,df])
+#print(df)
+next_button = driver.find_element_by_xpath('//*[@id="screener-content"]/table/tbody/tr[3]/td/table/tbody/tr/td[5]/a')
+next_button.click()
 
+#tb = driver.find_element_by_xpath('//*[@id="screener-content"]/table/tbody/tr[4]/td/table')
+#html_tb = tb.get_attribute('innerHTML')
+#html_tb = '<table>' + html_tb + '</table>'
+#df = pd.read_html(html_tb)
+#tb = soup.find_all('tbody')
+#screener-content > table > tbody > tr:nth-child(4) > td > table
 
 #table = driver.find_element_by_xpath('//*[@id="screener-content"]/table/tbody/tr[4]/td/table')
 #print(table.text)
