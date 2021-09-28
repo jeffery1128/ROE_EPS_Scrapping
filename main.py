f6c3816981f849e971ffc73373c43c504c1965b5
jeffery1128 <chunhotse3-c@my.cityu.edu.hk>
2021-09-28 17:08:10 +0800

    try to scrap eps % from trading view

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+import datetime
 
-
-cat = pd.read_csv('./stock_sector.csv')
+cat = pd.read_csv('./stock_list.csv')
 cat = cat.set_index('Ticker')
 driver = webdriver.Chrome()
+#get current year
+now_year = datetime.datetime.now().year
+eps_dict = {}
+eps_dict['ticker'] = ""
+for i in range(1,8):
+        eps_dict[now_year-i] = ""
+
+roe_dict = eps_dict
+
+print(eps_dict)
 
 def find_stock_cat(ticker):
         return cat.at[ticker,'Industry']
@@ -17,12 +27,25 @@ def find_same_cat(industry):
 
 def scrap_all_data(ticker):
         stock_list = find_same_cat(find_stock_cat(ticker)).tolist()
-        print((stock_list))
+        df = pd.DataFrame()
+        print(stock_list)
+        for ticker in stock_list:
+                print(ticker)
+                print(cat.at[ticker,'Exchange'])
+                url = "https://www.tradingview.com/symbols/"+cat.at[ticker,'Exchange']+"-"+ticker+"/financials-income-statement/earnings-per-share-diluted/"
+                driver.get(url)
+                eps_dict['ticker'] = ticker
+                for i in range(1,8):
+                        eps = driver.find_element_by_xpath('//*[@id="js-category-content"]/div[2]/div[2]/div[2]/div/div/div[2]/div[3]/div['+str(i+2)+']/div[4]')
+                        eps_dict[now_year-i] = eps.text
+                print(eps_dict)
+                time.sleep(0.3)
+
         
 
 
 
-scrap_all_data('TSLA')
+scrap_all_data('JPM')
 
 
 
